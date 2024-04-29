6971c8610cef6cd1779c1b7ad10434f909de01c6
whougie <whougie.c.lo@gmail.com>
2024-04-28 23:11:11 -0500

    Initial commit

---
+++ Develop/index.js
@@ -0,0 +1,31 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+
+  //Create inquirer to ask for title, description, installation instructions, usage information, contribution guidelines, and test instructions
+
+  // Using inquirer, ask which license to add then add a badge near the top of the README and add section called License and tell the application is covered by it
+
+  // Using inquirer, ask about GitHub username then add to the Questions section with the GitHub linnk to the profile
+
+  // Using inquirer, ask for email address then add to the Questions section if there any additional questions
+
+  // Build the section for the README 
+
+ // Build test instructions
+
+ // Build Contribution Guidelines
+
+ // Build a table of contents that is linked to its respectable section
+}
+
+// Function call to initialize app
+init();
